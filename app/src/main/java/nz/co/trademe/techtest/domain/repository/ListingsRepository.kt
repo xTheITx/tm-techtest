143acfe4a76a5ee6b85b5b90652f6f6e8c51cc82
Nic <ntolentino@juliusspencer.co.nz>
2019-05-05 17:41:23 +1200

    Updated ListingsRepository to return top category listings

--- app/src/main/java/nz/co/trademe/techtest/domain/repository/ListingsRepository.kt
+++ app/src/main/java/nz/co/trademe/techtest/domain/repository/ListingsRepository.kt
@@ -3,17 +3,27 @@ package nz.co.trademe.techtest.domain.repository
 import io.reactivex.Single
 import nz.co.trademe.techtest.data.network.NetworkRepository
 import nz.co.trademe.wrapper.models.ListedItemDetail
+import nz.co.trademe.wrapper.models.SearchCollection
+import nz.co.trademe.wrapper.models.SearchListing
 
 class ListingsRepository(
     private val networkRepository: NetworkRepository
 ) {
 
     /**
-     * Retrieve the details of a single listing
-     *
-     * @param listingId the listingId associated with the listing to return
+     * Returns the top list of [SearchListing] for the provided Category
      */
-    private fun getListings(listingId: Long): Single<ListedItemDetail> {
+    fun getTopCategoryListings(categoryNumber: String): Single<List<SearchListing>> {
+        return networkRepository.getTopCategoryListings(categoryNumber, 0)
+            .map { collection: SearchCollection ->
+                collection.list ?: ArrayList()
+            }
+    }
+
+    /**
+     * Returns the details the listing
+     */
+    fun getListingDetails(listingId: Long): Single<ListedItemDetail> {
         return networkRepository.getListing(listingId)
     }
 }
