c3853cd0469ee49c1e2f961731b5c0a4c8521134
Nic <ntolentino@juliusspencer.co.nz>
2019-05-07 22:24:20 +1200

    Removed unused class Resolved incorrect path

--- app/src/main/java/nz/co/trademe/techtest/data/TMResult.kt
+++
@@ -1,9 +0,0 @@
-package nz.co.trademe.techtest.data
-
-/**
- * Data class which represents a potentially nullable result
- * This class primarily exists to support RxJava's lack of support for null emissions
- */
-data class TMResult<T>(
-		val value: T?
-)
